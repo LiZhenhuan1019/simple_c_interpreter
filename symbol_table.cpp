d8c128355307e3f50ab4839950e24ccf143e071d
LiZhenhuan1019 <lizhenhuan1019@qq.com>
2016-10-24 12:26:47 +0800

    remove CMakeLists.txt

---
+++ symbol_table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lizhe on 2016/10/24.
+//
+
+#include "symbol_table.h"
---
+++ symbol_table.h
@@ -0,0 +1,16 @@
+//
+// Created by lizhe on 2016/10/24.
+//
+
+#ifndef SIMPLE_C_INTERPRETER_SYMBOL_TABLE_H
+#define SIMPLE_C_INTERPRETER_SYMBOL_TABLE_H
+#include <map>
+#include <string>
+class symbol_table
+{
+private:
+    std::map<std::string,
+};
+
+
+#endif //SIMPLE_C_INTERPRETER_SYMBOL_TABLE_H
